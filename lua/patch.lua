bf000a317f8f498394e9b590dd0668dc1109f5fc
Iris Morelle <shadowm@wesnoth.org>
2018-07-09 22:01:14 -0400

    lua/patch: Make several WML actions instant or no-ops when skipping messages
    This makes it possible to skip cutscenes much faster and without
    (most) artificial delays. The list of affected actions is such that none
    of the actions considered have side-effects on the game or RNG state.

--- lua/patch.lua
+++ lua/patch.lua
@@ -137,3 +137,89 @@ function wesnoth.wml_actions.message(cfg)
 
 	_WA_message(cfg)
 end
+
+---
+-- Extends several animation actions so that they do not trigger when the user
+-- is skipping messages.
+---
+
+-- scroll, scroll_to, scroll_to_unit are not included here since they are
+-- sometimes used to change the viewport position permanently after an event,
+-- with the intention of aiding the player in deciding what their next course
+-- of action should be. However, scroll_to and scroll_to_unit can become warp
+-- actions.
+
+local warp_actions = {
+	"scroll_to",
+	"scroll_to_unit",
+	-- Special cases made to make fade_out_music and fade_out_sound_effects
+	-- instantly mute either.
+	"fade_out_music",
+	"fade_out_sound_effects",
+	-- fade_in_sound_effects needs to instantly raise volume to 100%.
+	"fade_in_sound_effects",
+}
+
+local skippable_actions = {
+	"move_unit_fake",
+	"move_units_fake",
+	"animate_unit",
+	"sound",
+	"delay",
+	-- The lack of pauses can turn [color_adjust] into a health hazard, so
+	-- skip that as well.
+	"color_adjust",
+	"floating_text",
+}
+
+local _WA_warp_actions = {}
+local _WA_skip_actions = {}
+
+for i, action_id in ipairs(warp_actions) do
+	log_patch(action_id, "Warp on user skipping messages")
+
+	_WA_warp_actions[action_id] = wesnoth.wml_actions[action_id]
+	wesnoth.wml_actions[action_id] = function(cfg)
+		if wesnoth.is_skipping_messages() then
+			wprintf(W_INFO, "Ignoring timing or scrolling delay for [%s] while skipping [message]", action_id)
+
+			if action_id == "fade_out_music" then
+				wesnoth.set_music {
+					name      = "silence.ogg",
+					immediate = true,
+					append    = false,
+				}
+				-- HACK: give the new track a chance to start playing silently before
+				--       resetting to full volume.
+				wesnoth.delay(10)
+				wesnoth.music_list.volume = 100.0
+			elseif action_id == "fade_out_sound_effects" then
+				wesnoth.sound_volume(0)
+			elseif action_id == "fade_in_sound_effects" then
+				wesnoth.sound_volume(100)
+			else -- scroll_to* actions
+				local p = helper.parsed(cfg)
+				p.immediate = true
+				_WA_warp_actions[action_id](p)
+			end
+
+			return
+		end
+
+		_WA_warp_actions[action_id](cfg)
+	end
+end
+
+for i, action_id in ipairs(skippable_actions) do
+	log_patch(action_id, "Skip on user skipping messages")
+
+	_WA_skip_actions[action_id] = wesnoth.wml_actions[action_id]
+	wesnoth.wml_actions[action_id] = function(cfg)
+		if wesnoth.is_skipping_messages() then
+			wprintf(W_INFO, "Ignoring [%s] while skipping [message]", action_id)
+			return
+		end
+
+		_WA_skip_actions[action_id](cfg)
+	end
+end
