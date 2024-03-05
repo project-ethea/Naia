df6f9f9709e9808a686db947511670eb94cb2d15
Iris Morelle <shadowm@wesnoth.org>
2024-03-05 05:46:27 -0300

    New Year copyright update

--- lua/gui/bug.lua
+++ lua/gui/bug.lua
@@ -2,7 +2,7 @@
 -- Bug check report dialog
 --
 -- codename Naia - Project Ethea phase 1 campaigns shared library
--- Copyright (C) 2006 - 2023 by Iris Morelle <shadowm@wesnoth.org>
+-- Copyright (C) 2006 - 2024 by Iris Morelle <shadowm@wesnoth.org>
 --
 -- See COPYING for usage terms.
 --
