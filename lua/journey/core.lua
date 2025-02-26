120ad1eb0217201d382ff1f9c05a9e880489bcb7
Iris Morelle <shadowm@wesnoth.org>
2025-02-26 06:16:13 -0300

    lua/journey: Refactor

--- lua/journey/core.lua
+++ lua/journey/core.lua
@@ -1,8 +1,8 @@
 --
--- Wesnoth Journey Log module (back-end)
+-- Wesnoth Journey Log module (back-end) - Core API
 --
 -- codename Naia - Project Ethea phase 1 campaigns shared library
--- Copyright (C) 2024 by Iris Morelle <shadowm@wesnoth.org>
+-- Copyright (C) 2024 - 2025 by Iris Morelle <iris@irydacea.me>
 --
 -- See COPYING for usage terms.
 --
@@ -130,11 +130,6 @@ function journeylog.register_campaign_mnemonic(campaign_id, mnemonic)
 	journeylog_campaign_mn[campaign_id] = mnemonic
 end
 
-local function journeylog_var_init()
-	local entry_path = ("%s.%s.%s.%s"):format(JOURNEYLOG_TABLE, wesnoth.scenario.campaign.id, wesnoth.scenario.id, event_context_id())
-	return entry_path
-end
-
 local function _enumerate_impl_common(wml_container)
 	local enum = {}
 
@@ -160,112 +155,3 @@ function journeylog.enumerate_scenarios(campaign_id)
 	return _enumerate_impl_common(scenario_table)
 end
 
-function journeylog.read_scenario(campaign_id, scenario_id)
-	local var_path = ("%s.%s.%s"):format(JOURNEYLOG_TABLE, campaign_id, scenario_id)
-	local scenario_ct = wml.variables[var_path]
-
-	if not scenario_ct then
-		jprintf(W_ERR, "requested unrecorded campaign or scenario %s.%s", campaign_id, scenario_id)
-		return nil
-	end
-
-	local scenario_log = {}
-
-	-- We split the scenario log into groups for each event handler. We assume
-	-- that messages from event handlers with identical context hashes should
-	-- be grouped together as if they were part of a single sequence.
-
-	for i, cfg in ipairs(scenario_ct) do
-		local messages = {}
-		local event_hash = cfg[1]
-
-		jprintf(W_DBG, "reading messages in %s.%s event %s", campaign_id, scenario_id, event_hash)
-
-		for message in wml.child_range(cfg[2], "message") do
-			-- Just copy the WML contents, we don't do anything fancier here
-			-- (yet).
-			table.insert(messages, {
-				is_narrator = message.is_narrator,
-				speaker = message.speaker,
-				message = message.message,
-				image = message.image,
-				choice = message.choice,
-			})
-		end
-
-		table.insert(scenario_log, messages)
-	end
-
-	return scenario_log
-end
-
-local function journeylog_record_message(params_cfg, choice_text)
-	local event_path = journeylog_var_init()
-	local recorded_num = wml.variables[event_path .. ".message.length"] or 0
-
-	local record_path = ("%s.message[%d]"):format(event_path, recorded_num)
-
-	-- Stamp the event name into the message block for use by the UI
-	if not wml.variables[event_path] then
-		wml.variables[event_path] = { event_name = wesnoth.current.event_context.name }
-	end
-
-	wml.variables[record_path] = {
-		is_narrator = not params_cfg.title or nil,
-		speaker = params_cfg.title,
-		message = params_cfg.message,
-		image = params_cfg.portrait,
-		choice = choice_text,
-	}
-end
-
---
--- Wesnoth engine call hooks
---
-
-local ENG_gui_show_narration = gui.show_narration
-local ENG_wesnoth_interface_is_skipping_messages = wesnoth.interface.is_skipping_messages
-
-jprintf(W_DBG, "hook wesnoth.interface.is_skipping_messages")
-wesnoth.interface.is_skipping_messages = function(i_am_naia)
-	-- We pretend the user is never skipping messages when called by functions
-	-- outside JourneyLog so that we can always capture the message that would
-	-- be shown by gui.show_narration for safekeeping. The only exception is
-	-- Naia, which needs to query the actual skipping state in order to adjust
-	-- the behaviour of various functions including visual effects.
-	if i_am_naia ~= 0x4149414E then
-		--jprintf(W_DBG, "foreign code queried user skip, lying and saying no")
-		return false
-	end
-
-	-- Secret handshake from Naia!
-	--jprintf(W_DBG, "Naia queried user skip")
-	return ENG_wesnoth_interface_is_skipping_messages()
-end
-
-jprintf(W_DBG, "hook gui.show_narration")
-gui.show_narration = function(msg_cfg, options, text_input)
-	jprintf(W_DBG, "in substitute gui.show_narration()")
-
-	local res, user_text = -1, nil
-
-	if not ENG_wesnoth_interface_is_skipping_messages() or text_input ~= nil or #options > 0 then
-		jprintf(W_DBG, "jumping to engine to display [message]")
-		-- We capture the engine's results since these will be used by our caller to
-		-- act upon the user's choice, including enabling skip mode. If user input is
-		-- involved, we need to record it in JourneyLog as well.
-		res, user_text = ENG_gui_show_narration(msg_cfg, options, text_input)
-	else
-		jprintf(W_DBG, "skipping UI display of [message] due to user skip")
-	end
-
-	if user_text ~= nil then
-		journeylog_record_message(msg_cfg, user_text)
-	elseif #options > 0 and res > 0 then
-		journeylog_record_message(msg_cfg, options[res].label)
-	else
-		journeylog_record_message(msg_cfg)
-	end
-
-	return res, user_text
-end
