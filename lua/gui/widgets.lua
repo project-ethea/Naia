60a7e3c1a8fd2cb804443175648f74e9286ae5c3
Iris Morelle <shadowm@wesnoth.org>
2025-02-25 23:49:54 -0300

    lua/gui: Add functionality for portrait buttons in JourneyLog

--- lua/gui/widgets.lua
+++ lua/gui/widgets.lua
@@ -14,9 +14,50 @@ local _ = wesnoth.textdomain "wesnoth-Naia"
 
 -- Constants and helpers
 
-local JOURNEYLOG_PANEL_PADDING = 3
+local JOURNEYLOG_BIO_PORTRAIT_SIZE = 200
 
+local JOURNEYLOG_PANEL_PADDING = 3
 local JOURNEYLOG_PANEL_BORDER_COLOR = "114, 79, 46, 127" -- GUI__BORDER_COLOR_DARK
+
+--
+-- Helper to build a round border around the canvas edges.
+--
+local function C_round_frame(params)
+	local color = params.color or "0, 0, 0, 255"
+	local thickness = params.thickness or 1
+	return T.line {
+		x1 = 1,
+		y1 = 0,
+		x2 = "(width - 2)",
+		y2 = 0,
+		thickness = thickness,
+		color = color
+	},
+	T.line {
+		x1 = 0,
+		y1 = 1,
+		x2 = 0,
+		y2 = "(height - 2)",
+		thickness = thickness,
+		color = color
+	},
+	T.line {
+		x1 = 1,
+		y1 = "(height - 1)",
+		x2 = "(width - 2)",
+		y2 = "(height - 1)",
+		thickness = thickness,
+		color = color
+	},
+	T.line {
+		x1 = "(width - 1)",
+		y1 = 1,
+		x2 = "(width - 1)",
+		y2 = "(height - 2)",
+		thickness = thickness,
+		color = color
+	}
+end
 
 -- Canvas definition WML
 
@@ -182,42 +223,87 @@ gui.add_widget_definition("panel", "naia_journeylog_panel", {
 					border_color = "0, 0, 0, 255",
 					fill_color = "0, 0, 0, 127" -- GUI__BACKGROUND_COLOR_ENABLED
 				},
-				T.line {
-					x1 = 1,
-					y1 = 0,
-					x2 = "(width - 2)",
-					y2 = 0,
-					thickness = 1,
-					color = JOURNEYLOG_PANEL_BORDER_COLOR
-				},
-				T.line {
-					x1 = 0,
-					y1 = 1,
-					x2 = 0,
-					y2 = "(height - 2)",
-					thickness = 1,
-					color = JOURNEYLOG_PANEL_BORDER_COLOR
-				},
-				T.line {
-					x1 = 1,
-					y1 = "(height - 1)",
-					x2 = "(width - 2)",
-					y2 = "(height - 1)",
-					thickness = 1,
-					color = JOURNEYLOG_PANEL_BORDER_COLOR
-				},
-				T.line {
-					x1 = "(width - 1)",
-					y1 = 1,
-					x2 = "(width - 1)",
-					y2 = "(height - 2)",
-					thickness = 1,
-					color = JOURNEYLOG_PANEL_BORDER_COLOR
-				},
+				C_round_frame({ color = JOURNEYLOG_PANEL_BORDER_COLOR })
 			}
 		},
 		T.foreground {
 			T.draw {}
+		}
+	}
+})
+
+local function journeylog_bio_portrait_canvas_image(image_func)
+	return T.image {
+		name = "('[text][ipf]' where ipf = '" .. (image_func or "") .. "')",
+		--name = "(text)",
+		x = "(if(image_original_width < width, (width - image_original_width)/2, 0))",
+		y = "(if(image_original_width < height, (height - image_original_height)/2, 0))",
+		-- BIG TODO: scale images proportionally if they don't fit
+		w = "(min(image_original_width, width) * image_original_width/image_original_height)",
+		h = "(min(image_original_height, height))",
+		resize_mode = "scale"
+	}
+end
+
+local function journeylog_bio_portrait_canvas(params)
+	local border = params.border or "114, 79, 46, 127"
+	local bg = params.bg or "0, 0, 0, 127"
+	local image_func = params.image_func or ""
+
+	return T.draw {
+		T.rectangle {
+			w = "(width)",
+			h = "(height)",
+			fill_color = bg
+		},
+		journeylog_bio_portrait_canvas_image(image_func),
+		T.rectangle {
+			x = 1,
+			y = 1,
+			w = "(width - 2)",
+			h = "(height - 2)",
+			border_thickness = 1,
+			border_color = "0, 0, 0, 255",
+		},
+		C_round_frame({ color = border })
+	}
+end
+
+gui.add_widget_definition("button", "naia_journeylog_image_viewer_button", {
+	id = "naia_journeylog_image_viewer_button",
+	description = "a decade and a half later, gui2 still sucks",
+
+	T.resolution {
+		min_width         = JOURNEYLOG_BIO_PORTRAIT_SIZE,
+		min_height        = JOURNEYLOG_BIO_PORTRAIT_SIZE,
+		default_width     = JOURNEYLOG_BIO_PORTRAIT_SIZE,
+		default_height    = JOURNEYLOG_BIO_PORTRAIT_SIZE,
+		max_width         = 0,
+		max_height        = JOURNEYLOG_BIO_PORTRAIT_SIZE,
+
+		text_extra_width  = 0,
+		text_extra_height = 0,
+		text_font_size    = 0,
+
+		T.state_enabled {
+			journeylog_bio_portrait_canvas({})
+		},
+		T.state_disabled {
+			journeylog_bio_portrait_canvas({
+				border = "79, 79, 79, 95",
+				image_func =  "~GS()~O(0.6)"
+			})
+		},
+		T.state_pressed {
+			journeylog_bio_portrait_canvas({
+				image_func = "~BLEND(0,0,0,0.08)"
+			})
+		},
+		T.state_focused {
+			journeylog_bio_portrait_canvas({
+				bg = "18, 24, 40, 127",
+				image_func = "~BLEND(255,255,255,0.02)"
+			})
 		}
 	}
 })
