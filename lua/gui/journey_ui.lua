project-ethea/Naia

commit 52bc39dc3595fc36e3af4b96963a43fdb7e8cbe7

journeylog: Implement search (highlight) within Knowledge pages
This uses a rather dumb case-insensitive search algorithm of my own
which is most definitely inefficient af. The comparisons are likely
non-functional for anything other than ASCII characters, but at least
it should all be Unicode-safe -- meaning it won't produce invalid UTF-8.

Really, the biggest inefficiency is the fact that to simplify the code
we currently procure a giant table of Unicode codepoints rather than
iterate over the existing string. I attempted an edit where we keep the
Unicode tables around forever, but it didn't make a noticeable impact on
memory usage (on macOS at least) suggesting that Lua's GC is smart
enough to make my optimization kind of pointless -- which is why it's
not part of this patch.

Keyboard smashing into the Search box seems to grow memory usage by 300
MB after a while, but I'm not too sure how much of it can be attributed
to my Lua code and how much to GUI2 itself; case in point, memory usage
also grows when keyboard smashing into the Search box for the dialogue
tab, which doesn't indulge in all the complicated nonsense we do here.

It is mildly concerning for prospective mobile users (Android and iOS)
but it might be best to wait for the bug reports instead of prematurely
optimizing this yucky code.

lua/gui/journey_ui.lua
@@ -1961,6 +1961,55 @@ function journeylog_ui()
 		title = "",
 	}
 
+	local lore_page = {
+		text   = "",
+		widget = nil,
+	}
+
+	lore_page.set = function(self, text, widget)
+		self.text   = transform_markup(text) or JOURNEYLOG_UI_BIO_PLACEHOLDER
+		self.widget = widget
+		for i = #self, 1, -1 do
+			table.remove(self, i)
+		end
+	end
+
+	lore_page.clear = function(self)
+		self:set("", nil)
+	end
+
+	lore_page.push = function(self, text, widget)
+		table.insert(self, {
+			text   = transform_markup(text) or JOURNEYLOG_UI_BIO_PLACEHOLDER,
+			widget = widget
+		})
+	end
+
+	local function render_lore_text(self)
+		if (lore_page.text == "" or not lore_page.widget) and #lore_page == 0 then
+			return
+		end
+
+		local elements = {}
+
+		-- Single widget or multi widget mode?
+		if #lore_page == 0 then
+			elements = { lore_page }
+		else
+			elements = lore_page
+		end
+
+		for _, element in ipairs(elements) do
+			if element.widget == nil then
+				jprintf(W_ERR, "nil widget in lore page element set, this is a BUG")
+			else
+				element.widget.marked_up_text = highlight_marked_up_text(
+					element.text, self.search_box.text
+				)
+			end
+		end
+	end
+
 	local function show_chara_bio(self, index)
 		if not index or index > #archive.profiles then
 			self.archive_entry.visible = false
@@ -1978,8 +2027,10 @@ function journeylog_ui()
 
 		local page = self.archive_entry:add_item_of_type("chara_profile")
 
+		lore_page:set(profile.description, page.archive_entry_body)
+
 		page.archive_entry_title.marked_up_text = ("<big>%s</big>"):format(profile.name)
-		page.archive_entry_body.marked_up_text = transform_markup(profile.description) or JOURNEYLOG_UI_BIO_PLACEHOLDER
+		render_lore_text(self)
 
 		page.chara_portrait.label = profile.portrait or ""
 		page.race.marked_up_text = bio_race_name_helper(profile.race, profile.gender)
@@ -2035,8 +2086,10 @@ function journeylog_ui()
 
 		local page = self.archive_entry:add_item_of_type("lore_entry")
 
+		lore_page:set(entry.text, page.archive_entry_body)
+
 		page.archive_entry_title.marked_up_text = ("<big>%s</big>"):format(entry.title)
-		page.archive_entry_body.marked_up_text = transform_markup(entry.text) or JOURNEYLOG_UI_BIO_PLACEHOLDER
+		render_lore_text(self)
 
 		local source = entry.source
 		if source then
@@ -2082,8 +2135,10 @@ function journeylog_ui()
 
 		local prologue = self.archive_entry:add_item_of_type("lore_entry")
 
+		lore_page:clear()
+		lore_page:push(entry.text, prologue.archive_entry_body)
+
 		prologue.archive_entry_title.marked_up_text = ("<big>%s</big>"):format(entry.title)
-		prologue.archive_entry_body.marked_up_text = transform_markup(entry.text) or JOURNEYLOG_UI_BIO_PLACEHOLDER
 
 		prologue.source.visible = false
 		prologue.source_heading.visible = false
@@ -2099,8 +2154,9 @@ function journeylog_ui()
 		for i, section_data in ipairs(entry.sections) do
 			local section = self.archive_entry:add_item_of_type("recap_subsection")
 
+			lore_page:push(section_data.text, section.archive_entry_body)
+
 			section.archive_entry_title.marked_up_text = ("%s"):format(section_data.title)
-			section.archive_entry_body.marked_up_text = transform_markup(section_data.text) or JOURNEYLOG_UI_BIO_PLACEHOLDER
 
 			if section_data.quote ~= nil then
 				local quote_color = '#baac7d'
@@ -2118,6 +2174,8 @@ function journeylog_ui()
 				section.character_quote.visible = false
 			end
 		end
+
+		render_lore_text(self)
 
 		-- HACK: work around layout bug in Wesnoth 1.18 that causes the entry
 		-- display to often have unexpectedly short widgets when repopulated
@@ -2335,12 +2393,12 @@ function journeylog_ui()
 			self.archive_nav_tree:focus()
 			self.hidden_achievements.visible = false
 			self.compact_view.visible = false
-			self.search_box.visible = "hidden"
+			self.search_box.visible = true
 		elseif tab_num == 3 then
 			self.achievement_list:focus()
 			self.hidden_achievements.visible = naia_is_in_maintainer_mode() and wesnoth.game_config.debug
 			self.compact_view.visible = false
-			self.search_box.visible = "hidden"
+			--self.search_box.visible = "hidden"
 		end
 
 		initial_tab = tab_num
@@ -2434,7 +2492,12 @@ function journeylog_ui()
 		end
 
 		self.search_box.on_modified = function()
-			set_journey_filter(self, self.search_box.text)
+			local tab_num = self.log_section_selector.selected_index
+			if tab_num == 1 then
+				set_journey_filter(self, self.search_box.text)
+			elseif tab_num == 2 then
+				render_lore_text(self)
+			end
 		end
 
 		self.compact_view.on_modified = function()

lua/gui/utils.lua
@@ -7,6 +7,44 @@
 -- See COPYING for usage terms.
 --
 
+local UI_TEXT_HIGHLIGHT_BG_COLOR = "#153550" -- Same as [text_box] selection bg in 1.18
+
+-- Special characters that need to be escaped when used in a Pango markup
+-- context (cf. font::escape_text() in the game engine).
+local PANGO_SPECIAL_CHARS = {
+	["&"]  = "&amp;",
+	["<"]  = "lt;",
+	[">"]  = "gt;",
+	["'"]  = "&apos;",
+	['"']  = "&quot;",
+}
+
+local function unicode_array(s)
+	-- FIXME: Lua's UTF-8 support is advertised as very basic, so we aren't
+	-- getting normalized strings here. This might be a problem for other
+	-- languages. Waiting for the day someone files a bug report to see if I
+	-- can convince the Wesnoth devs to give us more tools to manipulate
+	-- Unicode strings in Lua.
+	return table.pack(utf8.codepoint(s, 1, -1))
+end
+
+local function unicode_tolower(char)
+	-- FIXME: Lua's Unicode support is so basic that this probably won't work
+	-- correctly for anything other than ASCII characters.
+	return utf8.codepoint(utf8.char(char):lower())
+end
+
+local function format_highlight(span)
+	return ("<span bgcolor='%s'>%s</span>"):format(UI_TEXT_HIGHLIGHT_BG_COLOR, span)
+end
+
+local function escape_text(text)
+	for original, sub in pairs(PANGO_SPECIAL_CHARS) do
+		text = text:gsub(original, sub)
+	end
+	return text
+end
+
 --
 -- Helper to make markup text fancier by converting certain tags to use color
 -- formatting.
@@ -23,4 +61,184 @@ function transform_markup(markup)
 	else
 		return nil
 	end
+end
+
+local U_HIGHLIGHT_PRE  = unicode_array("<span color='" .. UI_TEXT_HIGHLIGHT_BG_COLOR .. "'>")
+local U_HIGHLIGHT_POST = unicode_array("</span>")
+
+--
+-- Helper to add a highlighted background to text that matches the search
+-- terms. (Currently this is a full text search, albeit case-insensitive.)
+--
+function highlight_marked_up_text(text, search_for)
+	local function err(fmt, ...)
+		wprintf(W_ERR, "highlight_marked_up_text(): " .. fmt, ...)
+		return text
+	end
+
+	-- Get the trivial cases out of the way first
+
+	if type(text) ~= "string" then
+		return err("text is not a string")
+	end
+
+	if type(search_for) ~= "string" then
+		return err("query is not a string")
+	end
+
+	if text == "" or search_for == "" or #text < #search_for then
+		return text
+	end
+
+	-- Character counts prior to sanitization; we'll redo this aft4erwards
+	local u_text_len,   text_bad_pos   = utf8.len(text)
+	local u_search_len, search_bad_pos = utf8.len(search_for)
+
+	if not u_text_len then
+		return err("text contains invalid utf-8 at byte %d", text_bad_pos)
+	end
+	if not u_search_len then
+		return err("query contains invalid utf-8 at byte %d", search_bad_pos)
+	end
+
+	-- We can't apply lower() to text because we need the original case for
+	-- building the output, but we can apply lower() to the query
+	search_for = escape_text(search_for:lower())
+
+	local u_text   = unicode_array(text)
+	local u_search = unicode_array(search_for)
+
+	-- If character counts are identical perhaps strings are the same?
+	if u_text.n == u_search.n then
+		-- FIXME: probably not Unicode-aware
+		if text:lower() == search_for:lower() then
+			return format_highlight(text)
+		else
+			return text
+		end
+	end
+
+	-- Proceed with the full Unicode comparison
+
+	local query_pos   = 1
+
+	local tokens = {
+		cur_start     = 1,
+		cur_pos       = 1,
+		in_element    = "none",
+	}
+
+	tokens.push = function(highlight)
+		if tokens.cur_start <= 0 or
+		   tokens.cur_pos <= 0 or
+		   tokens.cur_pos < tokens.cur_start
+		then
+			return
+		end
+		assert(tokens.cur_start <= u_text.n)
+		assert(tokens.cur_pos   <= u_text.n)
+		local utf8_str = utf8.char(table.unpack(u_text, tokens.cur_start, tokens.cur_pos))
+		if highlight then
+			utf8_str = format_highlight(utf8_str)
+		end
+		table.insert(tokens, utf8_str)
+		tokens._advance_start()
+	end
+
+	tokens.push_highlighted = function(highlight_start)
+		local pos = tokens.cur_pos
+		-- Temporary rollback
+		tokens.cur_pos = highlight_start - 1
+		tokens.push()
+		tokens.cur_start = highlight_start
+		tokens.cur_pos = pos
+		tokens.push(true)
+		tokens._advance_start()
+	end
+
+	tokens.in_entity = function() return tokens.in_element == "ent"  end
+	tokens.in_tag    = function() return tokens.in_element == "tag"  end
+	tokens.in_plain  = function() return tokens.in_element == "none" end
+
+	tokens._advance_start = function() tokens.cur_start = math.min(tokens.cur_pos + 1, u_text.n + 1) end
+
+	tokens._start_element = function(el_type)
+		assert(tokens.in_plain())
+		tokens.cur_pos = tokens.cur_pos - 1
+		tokens.push() -- Push whatever precedes this element
+		tokens.cur_pos = tokens.cur_pos + 1
+		tokens.in_element = el_type
+		tokens.cur_start = tokens.cur_pos
+	end
+
+	tokens._end_element = function(el_type)
+		assert(tokens.in_element == el_type)
+		assert(tokens.cur_start > 0)
+		tokens.push()
+		tokens.in_element = "none"
+		tokens._advance_start()
+	end
+
+	tokens.start_entity = function() tokens._start_element("ent") end
+	tokens.end_entity   = function() tokens._end_element("ent")   end
+	tokens.start_tag    = function() tokens._start_element("tag") end
+	tokens.end_tag      = function() tokens._end_element("tag")   end
+
+	for pos = 1, u_text.n do
+		local char = u_text[pos]
+		tokens.cur_pos = pos
+
+		-- Skip characters
+		if tokens.in_tag() and char ~= string.byte('>') then
+			goto continue
+		end
+
+		-- Tags
+		if tokens.in_plain() and char == string.byte('<') then
+			-- Reset match because we went past a token boundary. Technically
+			-- we could just not do this so we can highlight things such as
+			-- "the <b>Demon Lord</b>" (for a query like "the demon lord"), BUT
+			-- we actually can't because we'd wind up with malformed Pango.
+			query_pos = 1
+			tokens.start_tag()
+			goto continue
+		elseif tokens.in_tag() and char == string.byte('>') then
+			query_pos = 1
+			tokens.end_tag()
+			goto continue
+		end
+
+		-- NOTE: Entities may crop up in the query value, so we treat them as
+		-- plain text for the purpose of tokenization
+
+		-- Check query match
+		local remaining_text_chars  = u_text.n - (pos - 1)
+		local remaining_query_chars = u_search.n - (query_pos - 1)
+
+		if remaining_query_chars <= remaining_text_chars then
+			if unicode_tolower(char) == unicode_tolower(u_search[query_pos]) then
+				if query_pos == u_search.n then
+					tokens.push_highlighted(pos - u_search.n + 1)
+				else
+					-- Continue match
+					query_pos = query_pos + 1
+					goto continue
+				end
+			end
+			-- No match or we just pushed one, reset match state
+			query_pos = 1
+		end
+		::continue::
+	end
+
+	-- Trailing plain element that didn't match query
+	if tokens.in_plain() and tokens.cur_start <= u_text.n then
+		tokens.push()
+	end
+
+	if tokens.in_tag() then
+		err("was in tag at the end, bad input or logic error?") -- continue anyway
+	end
+
+	return stringx.join(tokens, '')
 end

commit a77520bbc288fcffd400998515bb37910700d2b4

journeylog: Fix regression where Search is visible for Achievements tab
Introduced in 52bc39dc3595fc36e3af4b96963a43fdb7e8cbe7 due to what looks
like me testing something by commenting out code.

lua/gui/journey_ui.lua
@@ -1976,6 +1976,12 @@ function journeylog_ui()
 		-- Reset sidebar toggle every time we change view mode
 		self.layout_sidebar_toggle.selected = false
 		toggle_layout_visibility(self)
+
+		-- HACK: Render the search box hidden (takes up cell space) if in wide
+		-- layout and the Achievements tab is selected
+		if mode == "wide" and self.tabs_container.selected_index == 3 then
+			self.search_box.visible = "hidden"
+		end
 	end
 
 	local function journey_view_add_node(treeview, node_type, journey_msg)